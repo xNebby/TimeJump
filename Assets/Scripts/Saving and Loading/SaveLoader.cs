a2855fb7bf8d805669425318148facdb8352d2a5
xNebby <emmiew1709@gmail.com>
2023-11-06 18:58:07 +0000

    made the save loader script

---
+++ Assets/Scripts/Saving and Loading/SaveLoader.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SaveLoader : SingletonClass<SaveLoader>
+{
+
+    // Functions to add:
+    // Create save, Load save, Delete save.
+    // new game: Deletes save, then creates a new one, then loads it. 
+    // Continue: Loads game 
+    // Load game: Load into the level select. Enable the scene Async, passing along the save 
+
+
+}
